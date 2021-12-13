56f11b8cab37847af5319ca2c326234737a28699
muzoaydo <64156908+muzoaydo@users.noreply.github.com>
2021-12-13 14:39:52 +0300

    3 Modals Created and Exceptions Parsed

--- angular/src/app/auditlog/auditlog.component.ts
+++ angular/src/app/auditlog/auditlog.component.ts
@@ -12,15 +12,20 @@ export class AuditlogComponent implements OnInit {
 
   auditLogs = { items: [], totalCount: 0 } as PagedResultDto<AuditLogDto>;
 
-  
+  isExceptionsOpen = false;
+  isDetailsOpen = false;
+  isActionsOpen = false;
+  selectedException = ""
+
+
 
   auditArr = [
-    {label : "User", value : "userName", name : "::userName", prop : "userName", width : 30},
-    {label : "Ip Address", value : "clientIpAddress", name : "::clientIpAddress", prop : "clientIpAddress", width : 50},
-    {label : "Execution Time", value : "executionTime", name : "::executionTime", prop : "executionTime", width : 50},
-    {label : "Duration", value : "executionDuration", name : "::executionDuration", prop : "executionDuration", width : 10}
+    { label: "User", value: "userName", name: "::userName", prop: "userName", width: 30 },
+    { label: "Ip Address", value: "clientIpAddress", name: "::clientIpAddress", prop: "clientIpAddress", width: 50 },
+    { label: "Execution Time", value: "executionTime", name: "::executionTime", prop: "executionTime", width: 50 },
+    { label: "Duration", value: "executionDuration", name: "::executionDuration", prop: "executionDuration", width: 10 }
   ]
-  
+
 
   constructor(public readonly list: ListService, private auditLogService: AuditLogService) { }
 
@@ -29,9 +34,32 @@ export class AuditlogComponent implements OnInit {
 
     this.list.hookToQuery(auditLogStreamCreator).subscribe((response) => {
       this.auditLogs = response
-      
-    });
 
+      for (let i = 0; i < this.auditLogs.items.length; i++) {
+        let item = this.auditLogs.items[i];
+        if (item["exceptions"]) {
+          item["exceptions"] = JSON.stringify(JSON.parse(item["exceptions"]), null, 2).replace(/(\\r\\n|\r|\n)/gm, '');
+          debugger
+        }
+      }
+    }
+    )
+  };
+
+
+
+  exceptions(exception) {
+    this.isExceptionsOpen = true;
+    this.selectedException = exception;
+
+  }
+
+  details() {
+    this.isDetailsOpen = true;
+  }
+
+  actions() {
+    this.isActionsOpen = true;
   }
 
 }
